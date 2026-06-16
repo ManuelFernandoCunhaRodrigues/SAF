f5e0c15e46899ecd2f56c40ae5d3e33399e0b6b2
Manuelzinnn <manuelfernandocunharodrigues4@gmail.com>
2026-06-16 15:00:48 -0300

    fix: corrigir AuthProvider duplicado no frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import { AppRoutes } from "@/app/routes/app-routes";
-import { AuthProvider } from "@/app/providers/auth-provider";
 
 export default function App() {
-  return (
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
-  );
+  return <AppRoutes />;
 }
